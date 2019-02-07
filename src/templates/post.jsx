ef6ac3baed70374b44cf9609761dec17b8939aa6
1 <matome000001@gmail.com>
2019-02-07 19:57:37 +0900

    6th release

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -17,7 +17,7 @@ import "./b16-tomorrow-dark.css";
 import "./post.scss";
 
 import Postbody from "../components/Mycustom/Postbody"
-import alljson from "../../json_files/20190206-quotes.json"
+import alljson from "../../json_files/20190207-ariana-grande.json"
 
 export default class PostTemplate extends React.Component {
   constructor(props) {
