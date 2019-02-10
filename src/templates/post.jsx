4711c9a002ef86c6bf35b651b953820ed00880bb
1 <matome000001@gmail.com>
2019-02-11 01:25:10 +0900

    9th release

--- src/templates/post.jsx
+++ src/templates/post.jsx
@@ -17,7 +17,7 @@ import "./b16-tomorrow-dark.css";
 import "./post.scss";
 
 import Postbody from "../components/Mycustom/Postbody"
-import alljson from "../../json_files/20190209-hypergamy.json"
+import alljson from "../../json_files/20190210-parkwonsoon.json"
 
 export default class PostTemplate extends React.Component {
   constructor(props) {
